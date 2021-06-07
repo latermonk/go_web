d118ea4a5df270e4dc345c62e66366572860e64e
latermonk <iback.china@gmail.com>
2021-06-07 11:28:10 +0800

    udate

---
+++ ex17/README.md
@@ -0,0 +1,8 @@
+# HTTP
+
+```api
+POST  新增
+PUT   修改
+DEL   删除
+GET   查询
+```
--- ex17/main.go
+++ ex17/main.go
@@ -1,10 +1,58 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func main() {
 	r := gin.Default()
 
+	r.GET("/index", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"method": "GET",
+
+		})
+	})
+
+	r.POST("/index", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"method": "POST",
+		})
+	})
+
+	r.DELETE("/index", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"method": "DELETE",
+		})
+	})
+
+	r.PUT("/index", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"method" : "PUT",
+		})
+
+	})
+
+	r.Any("/user", func(c *gin.Context) {
+		switch c.Request.Method {
+		case "GET":
+			c.JSON(http.StatusOK, gin.H{"message":"GET"})
+		case http.MethodPost:
+			c.JSON(http.StatusOK, gin.H{"message": "POST"})
+
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"message": "ANY",
+
+		})
+	})
+
+
+	//r.NoRoute("", func(context *gin.Context) {
+	//
+	//})
 
 	r.Run(":9999")
 }
