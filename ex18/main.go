027e754fe234944fca79023d07c60d338b08b180
latermonk <iback.china@gmail.com>
2021-06-07 13:56:22 +0800

    Update main.go

--- ex18/main.go
+++ ex18/main.go
@@ -1,17 +1,32 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
+func indexHnadler(c *gin.Context){
+	c.JSON(http.StatusOK, gin.H{
+		"msg": "index",
+})
+}
+
+func m1(c *gin.Context) {
+	fmt.Println("IN m1 ... ")
+}
+
+
+func m2(c *gin.Context) {
+	fmt.Println("IN m2 ... ")
+}
+
+
+
 func main() {
 	r := gin.Default()
 
-	r.GET("/index", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"msg": "index",
-		})
-	})
+	r.GET("/index", m1, m2,  indexHnadler)
+
 	r.Run(":9999")
 }
