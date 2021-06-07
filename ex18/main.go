e8c79849d01d6f08de31b41578706da3d2c1c247
latermonk <iback.china@gmail.com>
2021-06-07 13:45:51 +0800

    Update main.go

--- ex18/main.go
+++ ex18/main.go
@@ -1,10 +1,17 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func main() {
 	r := gin.Default()
 
-
+	r.GET("/index", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"msg": "index",
+		})
+	})
 	r.Run(":9999")
 }
