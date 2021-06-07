a4e0f31aee847978d66172abf46a5dc9b74cefde
latermonk <iback.china@gmail.com>
2021-06-07 10:40:33 +0800

    ex16

--- ex16/main.go
+++ ex16/main.go
@@ -1,9 +1,29 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func main() {
 	r := gin.Default()
+	r.GET("/test", func(c *gin.Context) {
+		c.Redirect(http.StatusMovedPermanently, "https://weixin.sogou.com/")
+	} )
+
+	r.GET("/a", func(c *gin.Context) {
+
+		c.Request.URL.Path = "/b"
+
+		r.HandleContext(c)
+
+	})
+
+	r.GET("/b", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message":"b",
+		})
+	})
 
 	r.Run(":9999")
 }
