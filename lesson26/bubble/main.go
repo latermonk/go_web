ec7985781d726f90866c89ad43c18cfbeaba46ac
latermonk <iback.china@gmail.com>
2021-06-07 09:33:19 +0800

    change port to 19099

--- lesson26/bubble/main.go
+++ lesson26/bubble/main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"net/http"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"net/http"
 )
 
 var (
@@ -13,13 +13,13 @@ var (
 
 // Todo Model
 type Todo struct {
-	ID int `json:"id"`
-	Title string `json:"title"`
-	Status bool `json:"status"`
+	ID     int    `json:"id"`
+	Title  string `json:"title"`
+	Status bool   `json:"status"`
 }
 
-func initMySQL()(err error){
-	dsn := "root:root1234@tcp(127.0.0.1:13306)/db1?charset=utf8mb4&parseTime=True&loc=Local"
+func initMySQL() (err error) {
+	dsn := "root:root1234@tcp(127.0.0.1:13306)/bubble?charset=utf8mb4&parseTime=True&loc=Local"
 	DB, err = gorm.Open("mysql", dsn)
 	if err != nil {
 		return
@@ -35,7 +35,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	defer DB.Close()  // 程序退出关闭数据库连接
+	defer DB.Close() // 程序退出关闭数据库连接
 	// 模型绑定
 	DB.AutoMigrate(&Todo{})
 
@@ -47,7 +47,7 @@ func main() {
 	r.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", nil)
 	})
-	
+
 	// v1
 	v1Group := r.Group("v1")
 	{
@@ -63,9 +63,9 @@ func main() {
 			//if err!= nil {
 			//}
 			// 3. 反回响应
-			if err = DB.Create(&todo).Error;err != nil {
+			if err = DB.Create(&todo).Error; err != nil {
 				c.JSON(http.StatusOK, gin.H{"error": err.Error()})
-			}else{
+			} else {
 				c.JSON(http.StatusOK, todo)
 				//c.JSON(http.StatusOK, gin.H{
 				//	"code": 2000,
@@ -78,15 +78,15 @@ func main() {
 		v1Group.GET("/todo", func(c *gin.Context) {
 			// 查询todo这个表里的所有数据
 			var todoList []Todo
-			if err = DB.Find(&todoList).Error; err!= nil {
+			if err = DB.Find(&todoList).Error; err != nil {
 				c.JSON(http.StatusOK, gin.H{"error": err.Error()})
-			}else {
+			} else {
 				c.JSON(http.StatusOK, todoList)
 			}
 		})
 		// 查看某一个待办事项
 		v1Group.GET("/todo/:id", func(c *gin.Context) {
-			
+
 		})
 		// 修改某一个待办事项
 		v1Group.PUT("/todo/:id", func(c *gin.Context) {
@@ -96,14 +96,14 @@ func main() {
 				return
 			}
 			var todo Todo
-			if err = DB.Where("id=?", id).First(&todo).Error; err!=nil{
+			if err = DB.Where("id=?", id).First(&todo).Error; err != nil {
 				c.JSON(http.StatusOK, gin.H{"error": err.Error()})
 				return
 			}
 			c.BindJSON(&todo)
-			if err = DB.Save(&todo).Error; err!= nil{
+			if err = DB.Save(&todo).Error; err != nil {
 				c.JSON(http.StatusOK, gin.H{"error": err.Error()})
-			}else{
+			} else {
 				c.JSON(http.StatusOK, todo)
 			}
 		})
@@ -114,10 +114,10 @@ func main() {
 				c.JSON(http.StatusOK, gin.H{"error": "无效的id"})
 				return
 			}
-			if err = DB.Where("id=?", id).Delete(Todo{}).Error;err!=nil{
+			if err = DB.Where("id=?", id).Delete(Todo{}).Error; err != nil {
 				c.JSON(http.StatusOK, gin.H{"error": err.Error()})
-			}else{
-				c.JSON(http.StatusOK, gin.H{id:"deleted"})
+			} else {
+				c.JSON(http.StatusOK, gin.H{id: "deleted"})
 			}
 		})
 	}
