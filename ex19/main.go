8c8e9e0c94fbec70790606754e594ce8760cb3ad
latermonk <iback.china@gmail.com>
2021-06-08 09:39:41 +0800

    init

--- ex19/main.go
+++ ex19/main.go
@@ -1,15 +1,57 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	//"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+
+
+type UserInfo struct {
+	ID uint
+	Name string
+	Gender string
+	Hobby string
+}
+
 
 func main() {
 
 	//gin.ForceConsoleColor()
 	//gin.DisableConsoleColor()
+	db, err := gorm.Open("mysql", "root:root@(127.0.0.1:3306)/db1?charset=utf8mb4&parseTime=True&loc=Local")
+	if err != nil{
+		panic(err)
+	}
+	defer db.Close()
 
 
+	//Create table
+	db.AutoMigrate(&UserInfo{})
 
-	r := gin.Default()
+	//
+	u1 := UserInfo{1, "qimi", "nan", "wa yong"}
+	u2 := UserInfo{2, "沙河娜扎", "女", "足球"}
 
-	r.Run()
+	db.Create(&u1)
+	db.Create(&u2)
+
+	//get
+	var u UserInfo
+	db.First(&u)
+	fmt.Printf("u:%#v\n", u)
+	db.Last(&u)
+	fmt.Printf("u:%#v\n", u)
+
+	//
+	db.Model(&u).Update("hobby", "双色球")
+
+
+	//r := gin.Default()
+
+
+	db.Delete(&u)
+	//r.Run()
 }
--- lesson19/main.go
+++ lesson19/main.go
@@ -19,7 +19,7 @@ type UserInfo struct {
 
 func main() {
 	// 连接MySQL数据库
-	db, err := gorm.Open("mysql", "root:root1234@(127.0.0.1:13306)/db1?charset=utf8mb4&parseTime=True&loc=Local")
+	db, err := gorm.Open("mysql", "root:root@(127.0.0.1:3306)/db1?charset=utf8mb4&parseTime=True&loc=Local")
 	if err != nil {
 		panic(err)
 	}
@@ -29,15 +29,16 @@ func main() {
 	db.AutoMigrate(&UserInfo{})
 
 	// 创建数据行
-	u1 := UserInfo{1, "七米", "男", "蛙泳"}
+	u1 := UserInfo{1, "七米", "男",
+		"蛙泳"}
 	db.Create(&u1)
 	// 查询
 	var u UserInfo
 	db.First(&u)  // 查询表中第一天数据保存到u中
 	fmt.Printf("u:%#v\n", u)
 	// 更新
-	db.Model(&u).Update("hobby", "双色球")
+	//db.Model(&u).Update("hobby", "双色球")
 	// 删除
-	db.Delete(&u)
+	//db.Delete(&u)
 
 }
